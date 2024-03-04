de7a23b301f07d8f81c54d9f6d60bab71db7d211
olgapavlova <olgapavlova@gmail.com>
2024-03-04 11:46:55 +0300

    Window and List classes

--- main.py
+++ main.py
@@ -1,62 +1,33 @@
-import os
-from snaper import Page
-from pprint import pprint
-from datetime import datetime
-from selenium import webdriver
+from snaper import Window, List, Page
 # TODO Make unit tests
 
 # Config
 # TODO Move to separated file
 driver_path = "/home/op/Software/SeleniumChromeDriver"
-list_path = "lists/sites.txt"
 screenshots_path = "shots/"
 browser_width = 1440
-wait_in_seconds = 30
+wait_in_seconds = 50
+list_txt_file = "lists/sites.txt"
 
-# Dir to save shots
-screenshots_path = screenshots_path + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-os.mkdir(screenshots_path)
-screenshots_path = screenshots_path + "/"
-# TODO Add check if path exists (however, it is temp decision)
+# Browser window
+mywindow = Window(driver_path, wait_in_seconds)
+mywindow.start_driver()
+mywindow.set_window_width(browser_width)
 
-# Init webdriver
-options = webdriver.ChromeOptions()
-options.binary_location = driver_path
-driver = webdriver.Chrome()
-
-# Change browser width
-browser_height = driver.get_window_size()['height']
-driver.set_window_size(width=browser_width, height=browser_height)
-
-# Read list of URLs
-url_list = []
-with open(list_path, 'r') as list:
-    for line in list:
-        url_list.append(line.rstrip('\n'))
+# List object
+mylist = List(list_txt_file, mywindow)
+mylist.set_screenshot_dir(screenshots_path)
 
 # Go through the list
 try:
-    for url in url_list:
-
-        mydriver = Page(driver, wait_in_seconds)
-        mydriver.open_page(url)
-
-        mydriver.get_width_and_height()
-
-        mydriver.make_screenshot()
-
-        file_path = url.split("/")[2]
-        file_path = screenshots_path + file_path + '.png'
-
-        # Save image to file
-        with open(file_path, "wb") as file:
-            file.write(mydriver.image)
-
-        # driver.save_screenshot(file_path)
-        print(file_path)
-
+    for url in mylist.url_list:
+        mypage = Page(mylist, wait_in_seconds, url)
+        mypage.open_page()
+        mypage.get_width_and_height()
+        mypage.make_screenshot()
+        mypage.save_image_to_file()
 except Exception as ex:
     print(ex)
 finally:
-    driver.close()
-    driver.quit()
+    mywindow.driver.close()
+    mywindow.driver.quit()
--- snaper/__init__.py
+++ snaper/__init__.py
@@ -1,38 +1,91 @@
 import base64
+import os
+from selenium import webdriver
+from datetime import datetime
 
-# TODO Make List class
-# TODO Make Window class
+class Window:
+    '''Browser window, its config and operations'''
+    def __init__(self, driver_path, wait_in_seconds) -> None:
+        self.driver_path = driver_path
+        self.wait_in_seconds = wait_in_seconds
+
+    def start_driver(self):
+        options = webdriver.ChromeOptions()
+        options.binary_location = self.driver_path
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(self.wait_in_seconds)
+        # TODO Does it work here? Replace waiting by explicit one
+
+    def set_window_width(self, width):
+        height = self.driver.get_window_size()['height']
+        self.driver.set_window_size(width=width, height=height)
+
+class List:
+    '''List of URLs to make screenshots'''
+    def __init__(self, txt_file, mywindow) -> None:
+        self.txt_file = txt_file
+        self.url_list = self._set_url_list()
+        self.driver = mywindow.driver
+
+    def _set_url_list(self):
+        url_list = []
+        with open(self.txt_file, 'r') as list:
+            for line in list:
+                url_list.append(line.rstrip('\n'))
+        return url_list
+
+    def set_screenshot_dir(self, screenshots_path):
+        full_screenshots_path = screenshots_path + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        try:
+            os.mkdir(full_screenshots_path)
+        except Exception as ex:
+            print(ex)
+
+        self.screenshots_dir = full_screenshots_path + "/"
+        # TODO Add check if path exists (however, it is temp decision)
+
 
 class Page:
-
-    def __init__(self, driver, wait_in_seconds) -> None:
-        self._driver = driver
-        self._driver.implicitly_wait(wait_in_seconds)
-        # TODO Replace waiting by explicit one
-
-    def open_page(self, url):
+    '''One page from the list'''
+    def __init__(self, list, wait_in_seconds, url) -> None:
+        self.list = list  # TODO Divorce with List class to use separately
+        self._driver = list.driver
         self.url = url
-        self._driver.get(url)
+        self.file_name = self.url.split("/")[2] + ".png"
 
+    def open_page(self):
+        self._driver.get(self.url)
 
     def get_width_and_height(self):
-        '''Page width and height by Google DevTools'''
+        '''
+        Page width and height by Google DevTools
+        Method help: https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-getLayoutMetrics
+        '''
         try:
             metrics = self._driver.execute_cdp_cmd("Page.getLayoutMetrics", {})
-            self.width = metrics["contentSize"]["width"]
-            self.height = metrics["contentSize"]["height"]
+            self.width = metrics["cssContentSize"]["width"]
+            self.height = metrics["cssContentSize"]["height"]
         except Exception as ex:
             print(ex)
 
     def make_screenshot(self):
-        '''Page screenshot by Google DevTools'''
+        '''
+        Page screenshot by Google DevTools
+        Method description: https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-captureScreenshot
+        '''
         # TODO Add some more types of screenshots
         try:
             screnshot_raw_data = self._driver.execute_cdp_cmd("Page.captureScreenshot", \
                                                            {'format': 'png', 'captureBeyondViewport': True})
-            self.image = base64.decodebytes(bytes(screnshot_raw_data['data'], 'utf-8'))
+            self.image_data = base64.decodebytes(bytes(screnshot_raw_data['data'], 'utf-8'))
         except Exception as ex:
             print(ex)
+
+    def save_image_to_file(self):
+        '''Save image to .png file'''
+        file_path = self.list.screenshots_dir + self.file_name
+        with open(file_path, "wb") as file:
+            file.write(self.image_data)
 
 
 if __name__ == "__main__":
